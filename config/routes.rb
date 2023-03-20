82e67e19102929748ded962bebb8f55a14832eb9
“lukenilsson” <luke.nilsson@gmail.com>
2023-03-20 16:42:18 -0400

    create user / session controllers and HTML views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   resources :pets
+  get "/signup" => "users#new"
+  post "/users" => "users#create"
+
+  get "/login" => "sessions#new"
+  post "/sessions" => "sessions#create"
+  get "/logout" => "sessions#destroy"
 end
