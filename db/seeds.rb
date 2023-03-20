97f7fc1155d1d0737ed907812e3bff43a3bec9b9
“lukenilsson” <luke.nilsson@gmail.com>
2023-03-20 15:08:17 -0400

    Add model w seeds for inital data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# Pet.create(
+#   name: "Scout",
+#   breed: "Mix",
+#   image: "scout.jpg",
+# )
+# Pet.create(
+#   name: "Dakota",
+#   breed: "Lab",
+#   image: "dakota.jpg",
+# )
+# Pet.create(
+#   name: "Foxy",
+#   breed: "Schnauzer",
+#   image: "foxy.jpg",
+# )
